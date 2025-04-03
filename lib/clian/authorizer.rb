9ad4f4f7b8f6050fa5f78ae08c0447786c74852e
Yoshinari Nomura <nom@quickhack.net>
2025-04-03 19:56:51 +0900

    Receive OAuth code by Webrick

--- lib/clian/authorizer.rb
+++ lib/clian/authorizer.rb
@@ -22,22 +22,62 @@ module Clian
     end
 
     def auth_interactively(user_id = "default", shell = Thor.new.shell)
-      oob_uri = "urn:ietf:wg:oauth:2.0:oob"
+      callback_uri = "http://localhost:8000"
 
-      url = @authorizer.get_authorization_url(base_url: oob_uri)
+      url = @authorizer.get_authorization_url(base_url: callback_uri)
       begin
         Launchy.open(url)
       rescue
         puts "Open URL in your browser:\n #{url}"
       end
 
-      code = shell.ask "Enter the resulting code:"
+      puts "Waiting OAuth code at #{callback_uri} ..."
+
+      code = get_oauth_code_by_webserver(8000) ||
+             shell.ask("Enter the resulting code:")
+      puts "Done."
 
       @authorizer.get_and_store_credentials_from_code(
         user_id:  user_id,
         code:     code,
-        base_url: oob_uri
+        base_url: callback_uri
       )
     end
+
+    private
+
+    def get_oauth_code_by_webserver(port = 8000)
+      require 'webrick'
+      require 'uri'
+
+      code = nil
+      server = WEBrick::HTTPServer.new(
+        Port: port,
+        Logger: WEBrick::Log.new(File.open(File::NULL, "w")),
+        AccessLog: []
+      )
+
+      server.mount_proc '/' do |req, res|
+        query = URI.decode_www_form(req.query_string || "").to_h
+        code = query["code"]
+
+        if code
+          res.status = 200
+          res.content_type = "text/plain"
+          res.body = "Got OAuth code: #{code}\nYou can close this browser and back to CLI."
+
+          # shutdown server in another thread to avoid blocking current request
+          Thread.new { server.shutdown }
+        else
+          res.status = 400
+          res.body = "Failed to get OAuth code"
+        end
+      end
+
+      # block until shutdown is called in another thread
+      server.start
+      return code
+    end
+
   end # Authorizer
 end # Clian
